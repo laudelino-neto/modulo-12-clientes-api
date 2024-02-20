d1e921aa5b265a80cfae8e44e415120fe4fbc4c3
laudelino-neto <laudelino_sc@hotmail.com>
2024-02-20 16:57:16 -0300

    Inicio do controller de clientes

---
+++ src/main/java/br/com/unicesumar/modulo12/controller/ClienteController.java
@@ -0,0 +1,10 @@
+package br.com.unicesumar.modulo12.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+}
